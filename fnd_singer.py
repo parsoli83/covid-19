d2ab7ffdbaf1067e81ca57956930ed950365ab73
Parsa Soleimani <61251770+parsoli83@users.noreply.github.com>
2020-04-09 01:07:38 +0430

    Update fnd_singer.py

--- fnd_singer.py
+++ fnd_singer.py
@@ -1,16 +1,18 @@
 from __rOBOT_fUNCTIONS__ import *
+import random
 def find_singer():
     #you should put the list of singers and their songs here
     #it should be a 2-D list and like the below sentence
     #[[song name.mp3,singer]]
-    list_songs_and_singers=[[["AronAfshar_1.mp3"],"آرون افشار"],[["HamedBahram_1.mp3"],"حامد بهرام"],[["BehnamBani_1.mp3"],"بهنام یانی"]]
+    list_songs_and_singers=[[["AronAfshar_1"],"آرون افشار"],[["HamedBahram_1"],"حامد بهرام"],[["BehnamBani_1"],"بهنام یانی"]]
     while True:   
         list_artist=random.choice(list_songs_and_singers)
-        MusicPlay(random.choice(list_artists[0])+"()30")
+        MusicPlay(random.choice(list_artist[0])+"()30")
         Input=CompleteVoiceAnalyse()
-        if Input!=list_artists[1]:
+        if Input!=list_artist[1]:
             TTS("اشتباه بود","fa")
         elif Input=="بیرون":
             break
         else:
-            TTS("درست بود","fa")
+            TTS("متوجه نشدم")
+find_singer()
